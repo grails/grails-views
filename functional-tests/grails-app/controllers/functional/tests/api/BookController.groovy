38f433d8b32d86682d2b3a7e1c7d84d4cdddcffb
Colin Harrington <colin.harrington@gmail.com>
2016-05-20 14:47:25 -0500

    Added test around namespaced controllers

---
+++ functional-tests/grails-app/controllers/functional/tests/api/BookController.groovy
@@ -0,0 +1,15 @@
+package functional.tests.api
+
+import functional.tests.Book
+
+
+class BookController {
+
+    static namespace = 'api'
+
+    static responseFormats = ['json']
+
+    def index() {
+        respond new Book(title: 'API - The Shining')
+    }
+}
