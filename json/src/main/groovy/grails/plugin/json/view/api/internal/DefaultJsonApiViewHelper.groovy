2ce3e8ccb1f63b7342821d0f2902e29c8a7201f3
Colin Harrington <colin.harrington@gmail.com>
2016-08-23 02:29:39 -0500

    JSON API: Added support for the relationship attribute.  Property for including the jsonapi block, refactored attribute code to only include the simple Attributes

--- json/src/main/groovy/grails/plugin/json/view/api/internal/DefaultJsonApiViewHelper.groovy
+++ json/src/main/groovy/grails/plugin/json/view/api/internal/DefaultJsonApiViewHelper.groovy
@@ -9,6 +9,8 @@ import groovy.transform.CompileStatic
 import org.grails.datastore.gorm.GormEnhancer
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.model.PersistentProperty
+import org.grails.datastore.mapping.model.types.Association
+import org.grails.datastore.mapping.model.types.Simple
 
 /**
  * @Author Colin Harrington
@@ -18,19 +20,20 @@ class DefaultJsonApiViewHelper implements JsonApiViewHelper {
     JsonView view
     GrailsJsonViewHelper viewHelper
     String contentType = "application/vnd.api+json"
+    boolean exposeJsonApi = false
 
     public static final JsonOutput.JsonWritable NULL_OUTPUT = new JsonOutput.JsonWritable() {
         @Override
         Writer writeTo(Writer out) throws IOException {
-            out.write(JsonOutput.NULL_VALUE);
-            return out;
+            out.write(JsonOutput.NULL_VALUE)
+            return out
         }
     }
 
     public static final JsonOutput.JsonWritable NOOP_OUTPUT = new JsonOutput.JsonWritable() {
         @Override
         Writer writeTo(Writer out) throws IOException {
-            return out;
+            return out
         }
     }
 
@@ -50,17 +53,13 @@ class DefaultJsonApiViewHelper implements JsonApiViewHelper {
             @CompileStatic
             Writer writeTo(Writer out) throws IOException {
                 out.write(JsonOutput.OPEN_BRACE)
-                //jsonapi tag
-//                JsonOutput.JsonWritable jsonapi = jsonapiMember()
-//                jsonapi.writeTo(out)
-
+                if (exposeJsonApi) {
+                    jsonapiMember().writeTo(out)
+                }
                 renderData(object).writeTo(out)
-
                 out.write(JsonOutput.CLOSE_BRACE)
                 return out
             }
-
-
         }
         return jsonWritable
     }
@@ -71,7 +70,7 @@ class DefaultJsonApiViewHelper implements JsonApiViewHelper {
             @Override
             Writer writeTo(Writer out) throws IOException {
                 out.write(JsonOutput.toJson("data"))
-                out.write(JsonOutput.COLON);
+                out.write(JsonOutput.COLON)
 
                 PersistentEntity entity = findEntity(object)
                 out.write(JsonOutput.OPEN_BRACE)
@@ -88,21 +87,59 @@ class DefaultJsonApiViewHelper implements JsonApiViewHelper {
                 out.write(JsonOutput.toJson(linkGenerator.link(resource: object))) //TODO Use a linking strategy
 
                 if (entity.persistentProperties) {
-                    out.write(JsonOutput.COMMA)
-                    out.write(JsonOutput.toJson('attributes'))
-                    out.write(JsonOutput.COLON);
-                    out.write(JsonOutput.OPEN_BRACE)
+                    List<PersistentProperty> attributes = entity.persistentProperties.findAll{it instanceof Simple}
+                    if (attributes) {
+                        out.write(JsonOutput.COMMA)
+                        out.write(JsonOutput.toJson('attributes'))
+                        out.write(JsonOutput.COLON)
+                        out.write(JsonOutput.OPEN_BRACE)
 
-                    entity.persistentProperties.eachWithIndex { PersistentProperty persistentProperty, int idx ->
-                        out.write(JsonOutput.toJson(persistentProperty.name))
-                        out.write(JsonOutput.COLON);
-                        out.write(JsonOutput.toJson(((GroovyObject) object).getProperty(persistentProperty.name)))
-                        if (idx < entity.persistentProperties.size() - 1) {
-                            out.write(JsonOutput.COMMA)
+                        attributes.eachWithIndex { PersistentProperty persistentProperty, int idx ->
+                            out.write(JsonOutput.toJson(persistentProperty.name))
+                            out.write(JsonOutput.COLON)
+                            out.write(JsonOutput.toJson(((GroovyObject) object).getProperty(persistentProperty.name)))
+                            if (idx < attributes.size() - 1) {
+                                out.write(JsonOutput.COMMA)
+                            }
                         }
+                        out.write(JsonOutput.CLOSE_BRACE)
                     }
-                    //TODO relationships
-                    out.write(JsonOutput.CLOSE_BRACE)
+                    //TODO Links
+                    if (entity.associations) {
+                        out.write(JsonOutput.COMMA)
+                        out.write(JsonOutput.toJson('relationships'))
+                        out.write(JsonOutput.COLON)
+                        out.write(JsonOutput.OPEN_BRACE)
+                        entity.associations.eachWithIndex { Association association, int idx ->
+                            out.write(JsonOutput.toJson(association.name))
+                            out.write(JsonOutput.COLON)
+
+                            out.write(JsonOutput.OPEN_BRACE)
+                            out.write(JsonOutput.toJson("data"))
+                            out.write(JsonOutput.COLON)
+                            if (association.isList()) {
+                                out.write(JsonOutput.OPEN_BRACKET)
+                                //TODO Refactor and handle list of associations
+                                out.write(JsonOutput.CLOSE_BRACKET)
+
+                            } else {
+                                out.write(JsonOutput.OPEN_BRACE)
+
+                                out.write(JsonOutput.toJson('type'))
+                                out.write(JsonOutput.COLON)
+                                out.write(JsonOutput.toJson(association.associatedEntity.decapitalizedName))
+                                out.write(JsonOutput.COMMA)
+
+                                out.write(JsonOutput.toJson('id'))
+                                out.write(JsonOutput.COLON)
+                                out.write(JsonOutput.toJson(linkGenerator.link(resource: association))) //TODO Use a linking strategy
+
+                                out.write(JsonOutput.CLOSE_BRACE)
+                            }
+                            out.write(JsonOutput.CLOSE_BRACE)
+                        }
+                        out.write(JsonOutput.CLOSE_BRACE)
+                    }
                 }
                 out.write(JsonOutput.CLOSE_BRACE)
                 return out
@@ -112,27 +149,21 @@ class DefaultJsonApiViewHelper implements JsonApiViewHelper {
     }
 
     JsonOutput.JsonWritable jsonapiMember() {
-        if (false) {
-            JsonOutput.JsonWritable writable = new JsonOutput.JsonWritable() {
-                @Override
-                @CompileStatic
-                Writer writeTo(Writer out) throws IOException {
-                    out.write(JsonOutput.OPEN_BRACE)
-                    StreamingJsonBuilder builder = new StreamingJsonBuilder(out)
-                    builder.call {
-                        writeName("jsonapi")
-                        writeValue([version: '1.0'])
-                    }
-                    out.write(JsonOutput.CLOSE_BRACE)
-                    return out
+        JsonOutput.JsonWritable writable = new JsonOutput.JsonWritable() {
+            @Override
+            @CompileStatic
+            Writer writeTo(Writer out) throws IOException {
+                out.write(JsonOutput.OPEN_BRACE)
+                StreamingJsonBuilder builder = new StreamingJsonBuilder(out)
+                builder.call {
+                    writeName("jsonapi")
+                    writeValue([version: '1.0'])
                 }
-
+                out.write(JsonOutput.CLOSE_BRACE)
+                return out
             }
-
-            return writable
-        } else {
-            return NOOP_OUTPUT
         }
+        return writable
     }
 
     protected PersistentEntity findEntity(Object object) {
