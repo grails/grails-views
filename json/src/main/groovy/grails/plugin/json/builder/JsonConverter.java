3ee0b8b504914e6c9f60943d5362102306d5362d
jameskleeh <james.kleeh@gmail.com>
2016-10-25 15:26:54 -0400

    Add javadoc

--- json/src/main/groovy/grails/plugin/json/builder/JsonConverter.java
+++ json/src/main/groovy/grails/plugin/json/builder/JsonConverter.java
@@ -2,6 +2,12 @@ package grails.plugin.json.builder;
 
 import groovy.lang.Closure;
 
+/**
+ * An interface for custom converters to implement for service location
+ *
+ * @author James Kleeh
+ * @since 1.2.0
+ */
 public interface JsonConverter {
     Closure<? extends CharSequence> getConverter();
 
