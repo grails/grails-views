076bc61b646c7b42061f1f33aef763e708b248f9
jameskleeh <james.kleeh@gmail.com>
2017-01-06 17:30:14 -0500

    Load project sources before compile configuration in view compilation

---
+++ core/src/test/groovy/grails/views/GenericViewConfigurationSpec.groovy
@@ -0,0 +1,34 @@
+package grails.views
+
+import org.grails.config.CodeGenConfig
+import spock.lang.Specification
+
+class GenericViewConfigurationSpec extends Specification {
+
+
+    void "test setting of boolean"() {
+        given:
+        def testClass = new TestClass()
+        String yml = 'grails.views.json.compileStatic: false'
+        CodeGenConfig config = new CodeGenConfig()
+        config.loadYml(new ByteArrayInputStream(yml.bytes))
+
+        expect:
+        testClass.compileStatic
+        !testClass.useAbsoluteLinks
+
+        when: "no relevant properties in the config"
+        testClass.readConfiguration(config)
+
+        then: "the properties don't change from the defaults"
+        !testClass.compileStatic
+        !testClass.useAbsoluteLinks
+    }
+}
+
+class TestClass implements GenericViewConfiguration {
+    @Override
+    String getViewModuleName() {
+        "json"
+    }
+}
