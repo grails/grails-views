2dc1a071c09c5280cfdb26c314da3464e9267c40
jameskleeh <james.kleeh@gmail.com>
2017-01-09 14:27:38 -0500

    Publish snapshots on 1.x and master

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ rm -rf *.zip
 
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"
 
