1ad7eb8ab1b9df677174e8a6de0dc8b9a6013359
Nick Mitchell <mitch2na@gmail.com>
2017-08-25 19:33:34 -0400

    Clean up template resolve test

--- core/src/test/groovy/grails/views/resolve/TemplateResolveUtilsSpec.groovy
+++ core/src/test/groovy/grails/views/resolve/TemplateResolveUtilsSpec.groovy
@@ -1,32 +1,17 @@
 package grails.views.resolve
 
 import javassist.util.proxy.ProxyFactory
-import org.grails.datastore.mapping.keyvalue.mapping.config.KeyValueMappingContext
-import org.grails.datastore.mapping.model.MappingContext
 import spock.lang.Specification
 
 /**
  * Created by graemerocher on 19/05/16.
  */
 class TemplateResolveUtilsSpec extends Specification {
-    MappingContext mappingContext
-
-    def setup() {
-        mappingContext = {
-            def ctx = new KeyValueMappingContext("test")
-            ctx.setCanInitializeEntities(true)
-            return ctx
-        }()
-    }
-
 
     void "Test template name calculations"() {
         expect:
         TemplateResolverUtils.shortTemplateNameForClass(Object) == '/object/_object'
         TemplateResolverUtils.fullTemplateNameForClass(Object) == '/java/lang/_object'
-    }
-
-    class Test {
     }
 
     void "Test template name calculations for javassist proxy"() {
