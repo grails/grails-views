41e7e8063685d9d2771fa63a97e076fbd80305d4
sdelamo <sergio.delamo@softamo.com>
2018-12-20 12:35:30 +0100

    Revert "Temporary ignore JsonException: expecting '}'"
    Close: https://github.com/grails/grails-views/commit/64b74c6dbc3c448e8bf8914e7e25ce9a998ef004

--- json/src/test/groovy/grails/plugin/json/view/HalEmbeddedSpec.groovy
+++ json/src/test/groovy/grails/plugin/json/view/HalEmbeddedSpec.groovy
@@ -2,8 +2,6 @@ package grails.plugin.json.view
 
 import grails.gorm.annotation.Entity
 import grails.plugin.json.view.test.JsonViewTest
-import spock.lang.Ignore
-import spock.lang.Issue
 import spock.lang.Specification
 
 /**
@@ -146,8 +144,6 @@ json {
         result.jsonText == '{"_embedded":{"players":[{"_links":{"self":{"href":"http://localhost:8080/player/1","hreflang":"en","type":"application/hal+json"}},"_links":{"self":{"href":"http://localhost:8080/player/1","hreflang":"en","type":"application/hal+json"}},"name":"Cantona"}]},"total":1}'
     }
 
-    @Ignore
-    @Issue('https://github.com/grails/grails-views/issues/196')
     void "test hal render method for one-to-many associations"() {
 
         when:"A GSON view that renders hal.render(..) is rendered"
@@ -173,8 +169,6 @@ json hal.render(team)
         result.json.'_embedded'
     }
 
-    @Ignore
-    @Issue('https://github.com/grails/grails-views/issues/196')
     void "test hal embedded method for one-to-many associations"() {
         when:"A GSON view that renders hal.embedded(..) is rendered"
 
