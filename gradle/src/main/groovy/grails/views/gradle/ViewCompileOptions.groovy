dcd12c1c8d4c5194b5a4f4a66601fb5e408feef4
Puneet Behl <behlp@objectcomputing.com>
2021-10-12 21:50:28 +0530

    Make Gradle 7.2 compatible
    Correctly define @Input annotation on Task input
    Fixes grails/grails-core#12132

--- gradle/src/main/groovy/grails/views/gradle/ViewCompileOptions.groovy
+++ gradle/src/main/groovy/grails/views/gradle/ViewCompileOptions.groovy
@@ -1,15 +1,21 @@
 package grails.views.gradle
 
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.compile.GroovyForkOptions
 
 /**
  * @author Graeme Rocher
  * @since 1.0
  */
-class ViewCompileOptions {
+class ViewCompileOptions implements Serializable {
 
+    private static final long serialVersionUID = 0L;
+
+    @Input
     String encoding = "UTF-8"
 
+    @Nested
     GroovyForkOptions forkOptions = new GroovyForkOptions()
 
 }
--- gradle/src/main/groovy/grails/views/gradle/json/JsonViewCompilerTask.groovy
+++ gradle/src/main/groovy/grails/views/gradle/json/JsonViewCompilerTask.groovy
@@ -2,6 +2,7 @@ package grails.views.gradle.json
 
 import grails.views.gradle.AbstractGroovyTemplateCompileTask
 import groovy.transform.CompileStatic
+import org.gradle.api.tasks.Input
 
 /**
  * Concrete implementation that compiles JSON templates
@@ -11,16 +12,19 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class JsonViewCompilerTask extends AbstractGroovyTemplateCompileTask {
 
+    @Input
     @Override
     String getFileExtension() {
         "gson"
     }
 
+    @Input
     @Override
     String getScriptBaseName() {
         "grails.plugin.json.view.JsonViewTemplate"
     }
 
+    @Input
     @Override
     protected String getCompilerName() {
         "grails.plugin.json.view.JsonViewCompiler"
